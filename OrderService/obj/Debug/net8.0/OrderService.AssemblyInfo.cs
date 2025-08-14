a0b5c72f0964614bd930b74aecd2aa5522a3e6e7
Ad Soyad <email@example.com>
2025-08-15 00:16:41 +0300

    Final Fix: Update Ocelot hostnames for Azure Container Apps

--- OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
+++ OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+546a2f95ef27151e5cff8a2af2d126e645f478a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7819a479b55d2e84517cc6a9dded441542faac8")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
