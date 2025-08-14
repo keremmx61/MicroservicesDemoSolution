a75f9ad8876f6033ef4160dc0f99245486797ac0
Ad Soyad <email@example.com>
2025-08-15 00:41:37 +0300

    Url

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -3,15 +3,15 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:80"); 
+
 var ocelotConfig = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Docker"
     ? "ocelot.docker.json"
     : "ocelot.local.json";
-builder.Configuration.AddJsonFile(ocelotConfig, optional: false, reloadOnChange: true);
 
+builder.Configuration.AddJsonFile(ocelotConfig, optional: false, reloadOnChange: true);
 builder.Services.AddOcelot();
 
 var app = builder.Build();
-
 await app.UseOcelot();
-
 app.Run();
